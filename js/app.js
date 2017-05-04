21eb6f73ec44ce5fb68ff1aae9fd601982a0b2c3
MontserratLili <lauraliligm@gmail.com>
2017-05-03 19:24:50 -0500

    terminando applove con funcionalidad en js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+var modal = document.getElementById("modalId");
 
+ var modalImagen = document.getElementById("img01");
+ var captText = document.getElementById("caption");
+ var imagenes = [];
+ var spans = [];
+
+
+ for (var i = 0; i < 12; i++){
+   imagenes[i]=document.getElementsByClassName("imagenes")[i];
+   imagenes[i].addEventListener("click", mostrar);
+ }
+
+ function mostrar(){
+   modal.style.display = "block";
+   modalImagen.src = this.src;
+   captText.innerHTML = this.alt;
+ }
+
+ for (var i = 0; i < 12; i++){
+   spans[i]=document.getElementsByClassName("ocultar")[i];
+   spans[i].addEventListener("click", cerrar);
+ }
+
+ function cerrar(){
+   modal.style.display = "none";
+ }
 
